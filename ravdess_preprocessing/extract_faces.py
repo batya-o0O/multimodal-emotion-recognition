8132e1439212a85baf7423cdb4f642c8aea6c7b2
Ayan <80458910+batya-o0O@users.noreply.github.com>
2024-02-09 01:05:05 +0600

    Update extract_faces.py

--- ravdess_preprocessing/extract_faces.py
+++ ravdess_preprocessing/extract_faces.py
@@ -18,7 +18,7 @@ save_length = 3.6 #seconds
 save_avi = True
 
 failed_videos = []
-root = '/kaggle/input/RAVDESS'
+root = '/kaggle/input/ravdess'
 
 select_distributed = lambda m, n: [i*n//m + n//(2*m) for i in range(m)]
 n_processed = 0
